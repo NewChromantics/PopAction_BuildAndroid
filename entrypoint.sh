e791fac33c42820f2e9bbebc214292915ceb0691
thomasSDK <tsdkelly@protonmail.com>
2020-07-28 18:03:05 +0100

    copy header file to upload folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,7 @@ export SOURCE_ROOT=/github/workspace
 
 bash ${BUILD_TARGET_NAME}.Android/build.sh
 
+cp Source/${BUILD_TARGET_NAME}.h ${BUILD_TARGET_NAME}.Android/libs
+
 echo "::set-env name=UPLOAD_NAME::Android"
 echo "::set-env name=UPLOAD_DIR::${BUILD_TARGET_NAME}.Android/libs"
