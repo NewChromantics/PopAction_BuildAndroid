f8e8e9db8648da912250cec38bce3294c8fd202b
thomasSDK <tsdkelly@protonmail.com>
2020-07-28 12:51:15 +0100

    added _

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 ls
 
 export BUILD_TARGET_NAME=$1
-export SOURCEROOT=/github/workspace
-echo $SOURCEROOT
+export SOURCE_ROOT=/github/workspace
+echo $SOURCE_ROOT
 cd ${BUILD_TARGET_NAME}.Android
 bash build.sh
