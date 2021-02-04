e63a0a98b1fce2a9288754a9b07cac89488ce153
Graham Reeves <graham@grahamreeves.com>
2021-02-04 15:20:55 +0000

    Action now expects build script path, project path, and optionally extra build.sh arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,26 @@
 
 ls
 
-export BUILD_TARGET_NAME=$1
+# build script used to be ${BUILD_TARGET_NAME}.Android/build.sh
+# now specify directly
+export BUILD_SCRIPT_PATH=$1
+export BUILD_PROJECT_PATH=$2
+# https://stackoverflow.com/a/9057392/355753
+export ADDITIONAL_BUILD_ARGUMENTS="${@:3}"
 export SOURCE_ROOT=/github/workspace
 
+if [ ! -f "$BUILD_SCRIPT_PATH" ]; then
+    echo "Build Script path ($BUILD_SCRIPT_PATH arg1) file not found"
+    exit 1
+fi
 
-bash ${BUILD_TARGET_NAME}.Android/build.sh
+if [ "$BUILD_PROJECT_PATH" == "" ]; then
+	echo "Build project path ($BUILD_PROJECT_PATH argument 2) missing."
+	exit 2
+fi
+
+
+bash ${BUILD_SCRIPT_PATH} ${BUILD_PROJECT_PATH} ${ADDITIONAL_BUILD_ARGUMENTS}
 # error if build fails
 RESULT=$?
 if [[ $RESULT -ne 0 ]]; then
@@ -16,4 +31,4 @@ fi
 
 
 echo "UPLOAD_NAME=Android" >> "$GITHUB_ENV"
-echo "UPLOAD_DIR=${BUILD_TARGET_NAME}.Android/libs" >> "$GITHUB_ENV"
+echo "UPLOAD_DIR=${BUILD_PROJECT_PATH}/libs" >> "$GITHUB_ENV"
