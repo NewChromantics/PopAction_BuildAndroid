1a584c9244f8961c9278e1679c64ba9804d585a4
Graham Reeves <graham@grahamreeves.com>
2021-02-04 14:56:58 +0000

    Error if any build stages fail

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,22 @@ ls
 export BUILD_TARGET_NAME=$1
 export SOURCE_ROOT=/github/workspace
 
+
 bash ${BUILD_TARGET_NAME}.Android/build.sh
+# error if build fails
+RESULT=$?
+if [[ $RESULT -ne 0 ]]; then
+    echo "Build script returned $RESULT, exiting"
+	exit $RESULT
+fi
 
 cp Source/${BUILD_TARGET_NAME}.h ${BUILD_TARGET_NAME}.Android/libs
+# error if copy fails
+RESULT=$?
+if [[ $RESULT -ne 0 ]]; then
+    echo "copying header returned $RESULT, exiting"
+	exit $RESULT
+fi
 
 echo "UPLOAD_NAME=Android" >> "$GITHUB_ENV"
 echo "UPLOAD_DIR=${BUILD_TARGET_NAME}.Android/libs" >> "$GITHUB_ENV"
